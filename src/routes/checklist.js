534df0ab28be842f43d4741d5e11606f22d62fc1
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-09 22:58:37 +1000

    Aula - Listando dados do Model e Atualizacao e Remocao de Dados

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,30 +1,58 @@
 const express = require('express')
-
 const router = express.Router();
+const Checklist = require('../models/checklist');
+const checklist = require('../models/checklist');
 
-router.get('/', (req, res) => {
-    console.log('Olá');
+router.get('/', async(req, res) => {
+    try {
+        let checklist = await Checklist.find({})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(500).json(error)
+    }
     res.send();
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.status(200).send(req.body);
+router.get('/:id', async(req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`Id: ${req.params.id}`)
+router.post('/', async (req, res) => {
+    let { name } = req.body
+
+    try {
+        let checklist = await Checklist.create({ name })    
+        res.status(200).send(checklist);
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.put('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`PUT Id: ${req.params.id}`)
+router.put('/:id', async(req, res) => {
+    let { name } = req.body
+
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, {new: true})
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.json(422).json(error)
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`DELTE Id: ${req.params.id}`)
+router.delete('/:id', async(req, res) => {
+    let { name } = req.body
+
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id)
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.json(422).json(error)
+    }
 })
 
 module.exports = router;
