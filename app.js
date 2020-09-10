acf4e6222c55691e4a63349c34896422e7136fd2
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-10 12:23:57 +1000

    Aula EJS Configuracao com o Express

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
+
+const rootRouter = require('./src/routes/index')
 const checklistRouter = require('./src/routes/checklist')
 require('./config/database')
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', rootRouter)
 app.use('/checklists', checklistRouter);
 
 app.listen(3000, () => {
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router();
 const Checklist = require('../models/checklist');
-const checklist = require('../models/checklist');
 
 router.get('/', async(req, res) => {
     try {
